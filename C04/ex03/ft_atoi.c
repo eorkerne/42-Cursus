e105f729423f91d0869022567edd072e61b79f2f
Arroudani Maxance <arroudani.max@gmail.com>
2022-07-09 03:50:08 +0200

    test

---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int ft_atoi(char *str)
+{
+    int sign;
+    int nb;
+    int i;
+
+    i = 0;
+    nb = 0;
+    sign = 1;
+    while (str[i] == ' ' && str[i] == '\f' && str[i] == '\r'
+    && str[i] == '\n' && str[i] == '\t' && str[i] == '\v')
+        i++;
+    while (str[i] == '+' || str[i] == '-')
+    {
+        if (str[i] == '-')
+            sign = sign * -1;
+        i++;
+    }
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        nb = nb * 10 + (str[i] - 48);
+        i++;
+    }
+    return (sign * nb);
+}
+
+int main(void)
+{
+    char test[] = "---12345avc845";
+
+    printf("%d", ft_atoi(test));
+}
