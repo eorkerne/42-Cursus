ae79b7091e7ca3dc41b1176f4915b349e4bb9f4a
eorkerne <arroudani.max@gmail.com>
2022-10-07 14:16:14 +0200

    ft_strnstr

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: maarroud <maarroud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 12:44:50 by maarroud          #+#    #+#             */
-/*   Updated: 2022/10/07 13:24:55 by maarroud         ###   ########.fr       */
+/*   Updated: 2022/10/07 14:15:54 by maarroud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,20 +25,12 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	{
 		while (haystack[i + j] == needle[j] && haystack[i + j] != '\0')
 			j++;
+		if (i + j > len)
+			return (NULL);
 		if (needle[j] == '\0')
 			return ((char *)&haystack[i]);
 		i++;
 		j = 0;
 	}
-	return (NULL);
+	return (0);
 }
-
-// #include <stdio.h>
-
-// int main(void)
-// {
-// 	char haystack[30] = "aaabcabcd";
-
-// 	printf("%s\n", ft_strnstr(haystack, "cd", 8));
-// 	return(0);
-// }
