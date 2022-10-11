68b8529208df37355102a54922e363a415687137
eorkerne <arroudani.max@gmail.com>
2022-10-11 13:52:31 +0200

    strdup / strjoin / strtrim

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: maarroud <maarroud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 16:56:09 by maarroud          #+#    #+#             */
-/*   Updated: 2022/10/07 17:08:34 by maarroud         ###   ########.fr       */
+/*   Updated: 2022/10/11 13:17:11 by maarroud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ char	*ft_strdup(const char *s1)
 	int		i;
 
 	cpy = malloc(sizeof(char) * ft_strlen(s1) + 1);
-	i = 0;
 	if (!cpy)
 		return (0);
+	i = 0;
 	while (s1[i])
 	{
 		cpy[i] = s1[i];
--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,50 +6,85 @@
 /*   By: maarroud <maarroud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 13:42:57 by maarroud          #+#    #+#             */
-/*   Updated: 2022/10/08 15:56:05 by maarroud         ###   ########.fr       */
+/*   Updated: 2022/10/11 13:41:06 by maarroud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strtrim(char const *s1, char const *set)
+int	ft_countset(char const *s1, char const *set)
 {
-	char	*newstr;
-	size_t	newlen;
 	size_t	countset;
 	size_t	i;
 	size_t	j;
-	size_t	k;
 
 	i = 0;
-	j = 0;
 	countset = 0;
-	while (s1[i] && set[j])
-	{
-		if (s1[i] != set[j])
-			countset++;
-		i++;
-		j++;
-	}
-	newlen = ft_strlen(s1) - countset;
-	newstr = malloc(sizeof(char) * newlen);
-	if (!newstr)
-		return (NULL);
-	i = 0;
-	k = 0;
-	while (s1[i] != '\0')
+	while (s1[i])
 	{
 		j = 0;
 		while (set[j])
 		{
-			while (s1[i] == set[j])
+			if (s1[i] == set[j])
 			{
-				i++;
+				countset++;
+				break ;
 			}
 			j++;
 		}
-		newstr[k++] = s1[i++];
+		i++;
 	}
-	newstr[k] = '\0';
+	return (countset);
+}
+
+char	*ft_replace(char const *s1, char const *set)
+{
+	char	*str2;
+	size_t	i;
+	size_t	j;
+
+	str2 = ft_strdup(s1);
+	i = 0;
+	while (str2[i])
+	{
+		j = 0;
+		while (set[j])
+		{
+			if (str2[i] == set[j])
+			{
+				str2[i] = '\0';
+				break ;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (str2);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*newstr;
+	char	*str2;
+	size_t	countset;
+	size_t	i;
+	size_t	j;
+
+	countset = ft_countset(s1, set);
+	str2 = ft_replace(s1, set);
+	newstr = malloc(sizeof(char) * (ft_strlen(s1) - countset) + 1);
+	if (!newstr)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (j < (ft_strlen(s1) - countset))
+	{
+		if (str2[i] == '\0')
+			i++;
+		else
+			newstr[j++] = str2[i++];
+	}
+	free(str2);
+	newstr[j] = '\0';
 	return (newstr);
 }
